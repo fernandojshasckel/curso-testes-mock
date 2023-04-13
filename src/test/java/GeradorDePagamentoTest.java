55def28c0ba0b6fbc13171e09aa1bb985adeb72c
fernandojshasckel <fernandojosehasckel@gmail.com>
2023-04-12 22:39:58 -0300

    005 - Capturando objetos com Mockito.

---
+++ src/test/java/GeradorDePagamentoTest.java
@@ -0,0 +1,71 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.service.GeradorDePagamento;
+
+public class GeradorDePagamentoTest {
+    
+    private GeradorDePagamento geradorDePagamento;
+
+    @Mock
+    private PagamentoDao pagamentoDao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.geradorDePagamento = new GeradorDePagamento(pagamentoDao);
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilao() {
+
+        Leilao leilao = leilao();
+        Lance lanceVencedor = leilao.getLanceVencedor();
+
+        geradorDePagamento.gerarPagamento(lanceVencedor);
+
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
+
+        Pagamento pagamento = captor.getValue();
+
+        assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+        assertEquals(lanceVencedor.getValor(), pagamento.getValor());
+        assertFalse(pagamento.getPago());
+        assertEquals(lanceVencedor.getUsuario(), pagamento.getUsuario());
+        assertEquals(leilao, pagamento.getLeilao());
+    }
+
+    private Leilao leilao() {
+        
+        Leilao leilao = new Leilao("Celular",
+            new BigDecimal("500"),
+            new Usuario("Fulano"));
+
+        Lance lanceVencedor = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+
+        leilao.propoe(lanceVencedor);
+        leilao.setLanceVencedor(lanceVencedor);
+
+        return leilao;
+    }
+    
+}
